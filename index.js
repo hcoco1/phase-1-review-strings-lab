f3f0d4b5264bb282680ea593cc93dc0923f297b6
Ivan Arias <arias.ivan@gmail.com>
2023-03-13 15:35:07 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Ivan Arias';
+const welcomeMessage = "Welcome to Flatbook, Ivan Arias!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, IVAN ARIAS!";
+const shortGreeting = "Welcome, I!";
